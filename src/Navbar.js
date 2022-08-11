6173256037fb7aa57d449ff3391cd404f2724ba2
rstr26 <77273388+rstr26@users.noreply.github.com>
2022-08-11 20:57:12 +0800

    axios test

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from 'react-router-dom'
 import './style/Navbar.css'
 import Search from "./Search";
+import axios from "axios";
 
 const Navbar = () => {
+
+    useEffect(() => {
+        (async () => {
+            await axios
+                .get('https://jsonplaceholder.typicode.com/posts')
+                .then(res => console.log(res.data))
+                .catch(err => console.log(err))
+        })()
+
+        return () => {
+
+        }
+    }, [])
     return(
         <div className="navbar">
             <nav className="col1">
