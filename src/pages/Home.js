f3e4048aa312516f797bca009a2e05a0c9e94dc3
rstr26 <77273388+rstr26@users.noreply.github.com>
2022-08-18 00:34:00 +0800

    api & carousel upd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import '../style/Home.css'
+import api from "../apiKey";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const Home = () => {
+    const [carouselList, setCarousel] = useState([])
 
     useEffect(() => {
-
+        (async => {
+            api.products.list({limit: 30})
+                .then(res => {
+                    setCarousel(res.data)
+                })
+                .catch(err => console.log(err))
+        })()
     }, [])
-    return(
-        <div className="home">
-            <div className="rowFlex">
-                <Carousel />
-                <NewItems />
+
+    if(carouselList.length === 0){
+        return(
+            <h1>Loading...</h1>
+        )
+    }
+    else{
+        return(
+            <div className="home">
+                <div className="row">
+                    <ImgCarousel items={carouselList}/>
+                    <NewItems />
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-const Carousel = (props) => {
+const ImgCarousel = (props) => {
+    const items = props.items
+    const [imgLinks, setLinks] = useState([])
+
+    useEffect(() => {
+        for(let i = 0; i < 5; i++){
+            setLinks(arr => [...arr, items[Math.floor(Math.random() * items.length)]])
+            //console.log(items[i].image.url)
+        }
+    }, [])
     return(
         <div className="carousel">
-            <h1>carousel</h1>
+            <div style={carouselStyle}>
+                <Carousel autoPlay={true}>
+                    {imgLinks.map((links, index) => {
+                        return(
+                            <img src={`${links.image.url}`} key={index}/>
+                        )
+                    })}
+                </Carousel>
+            </div>
         </div>
     )
 }
@@ -30,5 +65,10 @@ const NewItems = (props) => {
             <h1>new items</h1>
         </div>
     )
+}
+
+const carouselStyle = {
+    height: '300px',
+    width: '400px'
 }
 export default Home
